7fc2768804b1fd42962efd70bcb4a80b27909039
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-13 21:27:06 +0200

    Chapter 4 task4a

---
+++ src/initialization/PoorDogTestDrive.java
@@ -0,0 +1,10 @@
+package initialization;
+
+public class PoorDogTestDrive {
+
+    public static void main(String[] args) {
+        PoorDog one = new PoorDog();
+        System.out.println("Размер собаки: " + one.getSize());
+        System.out.println("Имя собаки: " + one.getName());
+    }
+}
---
+++ src/task4a/XCopy.java
@@ -0,0 +1,19 @@
+package task4a;
+
+public class XCopy {
+
+    public static void main(String[] args) {
+
+        int orig = 42;
+        int x;
+
+
+        XCopy x =x.go(orig);
+        System.out.println(orig + " " + y);
+    }
+
+    int go(int arg) {
+        arg = arg * 2;
+        return arg;
+    }
+}
