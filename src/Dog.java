da262a2b60f7216d1cedadfa1a26edcdb46e9f30
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-13 19:46:51 +0200

    Chapter 4 first task

---
+++ src/Dog.java
@@ -0,0 +1,34 @@
+public class Dog {
+    int size;
+    String name;
+
+    void bark(int numOfBarks) {
+        /*while (numOfBarks > 0) {
+            System.out.println("Гав!");
+            numOfBarks --;
+        }
+        if (size > 60) {
+            System.out.println("Гав! Гав!");
+
+        }else if (size > 14) {
+            System.out.println("Вуф! Вуф1");
+
+        }else {
+            System.out.println("Тяф! Тяф!");
+        }*/
+        while (numOfBarks > 0) {
+            if (size > 60) {
+                System.out.println("Гав! Гав!");
+
+            }else if (size > 14) {
+                System.out.println("Вуф! Вуф!");
+
+            } else {
+                System.out.println("Тяф! Тяф!");
+            }
+
+            numOfBarks--;
+        }
+
+    }
+}
